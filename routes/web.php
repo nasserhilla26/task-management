f92c2e56bca3ad7ab0c8b5c74d716bb35c21371e
Nasser Hilla <nasserhilla26@gmail.com>
2026-09-09 22:44:18 +0800

    Add task resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
+
+Route::resource('tasks', TaskController::class);
